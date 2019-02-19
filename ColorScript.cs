f42c0997d627a87dc62a4f328993420b82ba4e57
alexd448 <33429999+alexd448@users.noreply.github.com>
2019-02-19 15:19:25 -0500

    Add files via upload

---
+++ ColorScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+      GetComponent<Rigidbody>().velocity = new Vector3(2,2,5);
+    }
+
+    void OnMouseDown()
+    {
+      if (GetComponent<Rigidbody>().velocity.z < 0 && GetComponent<Transform>().position.z < 0 && GetComponent<Transform>().position.z >= - 3){
+        GetComponent<Rigidbody>().velocity = new Vector3(-3, 2, 8);
+      }
+      if (GetComponent<Rigidbody>().velocity.z < 0 && GetComponent<Transform>().position.z < -3 && GetComponent<Transform>().position.z >= - 6){
+        GetComponent<Rigidbody>().velocity = new Vector3(-1, 4, 8);
+      }
+      if (GetComponent<Rigidbody>().velocity.z < 0 && GetComponent<Transform>().position.z < -6 && GetComponent<Transform>().position.z >= -10){
+        GetComponent<Rigidbody>().velocity = new Vector3(1, 6, 8);
+      }
+    }
+
+
+
+    // Update is called once per frame
+    void OnCollisionEnter (Collision col)
+    {
+      if (col.gameObject.name == "Racket" && GetComponent<Rigidbody>().position.y <= 2){
+        GetComponent<Rigidbody>().velocity = new Vector3(-1, 8, -8);
+      }
+      if (col.gameObject.name == "Racket" && GetComponent<Rigidbody>().position.y > 2 && GetComponent<Rigidbody>().position.y <= 5){
+        GetComponent<Rigidbody>().velocity = new Vector3(-1, 6, -8);
+      }
+      if (col.gameObject.name == "Racket" && GetComponent<Rigidbody>().position.y > 5) {
+        GetComponent<Rigidbody>().velocity = new Vector3(-1, 4, -8);
+      }
+     }
+    void Update()
+    {
+
+    }
+}
---
+++ MatchMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatchMovement : MonoBehaviour
+{
+    public GameObject ball;
+    // Start is called before the first frame update
+    void Start()
+    {
+      ball = GameObject.Find("ballObj");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     if (ball.transform.position.y >= 0){
+        GetComponent<Transform>().position = new Vector3 (ball.transform.position.x, ball.transform.position.y, 10);
+      }
+    }
+}
